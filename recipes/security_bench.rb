6935e9e35a58e1c73172fd6cd3b0bce125229476
Rocklobster84 <thompson.stephanie84@gmail.com>
2022-02-25 09:36:13 -0600

    Fleshed out cookbook and default tests

--- recipes/security_bench.rb
+++ recipes/security_bench.rb
@@ -7,6 +7,44 @@
 # Allow log on locally set to administrators
 windows_user_privilege 'Allow log on locally' do
   privilege 'SeInteractiveLogonRight'
-  users          ['BUILTIN\Administrators']
-  action         :set
+  users ['BUILTIN\Administrators']
+  action :set
+end
+
+windows_user_privilege 'Allow log on through Remote Desktop Services' do
+  privilege 'SeInteractiveRemoteLogonRight'
+  users ['BUILTIN\Administrators', 'BUILTIN\Remote Desktop Users']
+  action: set
+end
+
+windows_security_policy 'EnableAdminAccount' do
+  secvalue '0'
+  action :set
+end
+
+registry_key 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' do
+  values [{
+    name: 'DisableCAD',
+    type: :dword,
+    data: 0,
+  }]
+  action :create
+end
+
+registry_key 'HKLM\SOFTWARE\Policies\Microsoft\W32Time\TimeProviders\NtpClient' do
+  values [{
+    name: 'Enabled',
+    type: :dword,
+    data: 1,
+  }]
+  action :create
+end
+
+registry_key 'HKLM\SOFTWARE\Policies\Microsoft\W32Time\TimeProviders\NtpServer' do
+  values [{
+    name: 'Enabled',
+    type: :dword,
+    data: 0,
+  }]
+  action :create
 end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -7,3 +7,40 @@
 describe security_policy do
   its('SeInteractiveLogonRight') { should eq ['S-1-5-32-544'] }
 end
+
+# Only administrators and remote desktop users should be able to login remotely
+describe security_policy do
+  its('SeRemoteInteractiveLogonRight') { should eq ['S-1-5-32-544', 'S-1-5-32-555'] }
+end
+
+#  The 'Accounts:Administrator account status' should exist and be disabled
+describe users.where { uid =~ /S\-1\-5\-21\-\d+\-\d+\-\d+\-500/ } do
+  it { should exist }
+end
+describe users.where { uid =~ /S\-1\-5\-21\-\d+\-\d+\-\d+\-500/ } do
+  it { should be_disabled }
+end
+
+#  The 'Interactive logon: do not require ctrl+alt+del' setting should exist and be disabled
+describe registry_key('HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System') do
+  it { should have_property 'DisableCAD' }
+end
+describe registry_key('HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System') do
+  its('DisableCAD') { should cmp == 0 }
+end
+
+#  The 'Enable Windows NTP client' setting should be exist and be enabled
+describe registry_key('HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient') do
+  it { should have_property 'Enabled' }
+end
+describe registry_key('HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient') do
+  its('Enabled') { should cmp == 1 }
+end
+
+#  The 'Enable Windows NTP server' setting should exist and be disabled
+describe registry_key('HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpServer') do
+  it { should have_property 'Enabled' }
+end
+describe registry_key('HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpServer') do
+  its('Enabled') { should cmp == 0 }
+end
